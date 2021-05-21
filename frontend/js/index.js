6e5d4780c42d9e6c2bf0cffd813db8a859758826
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-05-21 18:57:02 -0500

    added video and images, I did also added the user info to the thweet card

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -22,21 +22,24 @@ const buildTweets = (tweets, nextPage) => {
         twitterContent += `
             <div class="tweet-container">
                 <div class="tweet-user-info">
-                    <div class="tweet-user-profile">
+                    <div class="tweet-user-profile" style="background-image: url(${tweet.user.profile_image_url_https})">
 
                     </div>
                     <div class="tweet-user-name-container">
                         <div class="tweet-user-fullname">
-                            Jonnathan Garcia
+                            ${tweet.user.name}
                         </div>
                         <div class="tweet-user-username">
-                            @jonnathandor
+                            ${tweet.user.screen_name}
                         </div>
                     </div>
                 </div>
                 `;
-                if(tweet.extended_entities && tweet.extended_entities.media.length > 0){
-                    twitterContent += buildImages(tweet.extended_entities.media)
+                if(tweet.extended_entities 
+                    && tweet.extended_entities.media
+                    && tweet.extended_entities.media.length > 0){
+                    twitterContent += buildImages(tweet.extended_entities.media);
+                    twitterContent += buildVideo(tweet.extended_entities.media);
                 }
                 twitterContent += `
                 <div class="tweet-text-container">
@@ -65,6 +68,34 @@ const buildImages = (images) => {
     imagesContent += `</div>`
 
     return imageExists ? imagesContent : '';
+}
+
+const buildVideo = (mediaList) => {
+    let videoContent = `<div class="tweet-video-container">`;
+    let videoExists = false;
+    mediaList.map((media)=>{
+        if(media.type == "video" || media.type == 'animated_gif'){
+            videoExists = true;
+            const video = media.video_info.variants.find((video)=>video.content_type == 'video/mp4');
+            const videoOptions = getVideoOptions(media.type);
+            videoContent += `
+            <video ${videoOptions}>
+                <source src="${video.url}" type="video/mp4">
+                Your browser does not support HTML5 video.
+            </video>
+            `
+        }
+    })
+    videoContent += `</div>`;
+    return (videoExists ? videoContent : '');
+}
+
+const getVideoOptions = (mediaType) => {
+    if(mediaType == 'animated_gif'){
+        return "loop autoplay";
+    } else {
+        return "controls";
+    }
 }
 
 
